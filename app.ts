5f13d5289d5edb04cb1a371fbcefb19fa95116a5
nichi <kimpoychin131@gmail.com>
2026-08-16 14:29:22 +0000

    Stage 4: auth middleware and logout endpoint

--- app.ts
+++ app.ts
@@ -6,6 +6,15 @@ import { AuthTokenResponsePassword } from '@supabase/supabase-js';
 import e from 'express';
 dotenv.config();
 
+interface AuthRequest extends Request {
+  token?: string | undefined;
+  data?: {
+    id: string;
+    email: string | undefined;
+    account_created: string;
+  };
+}
+
 const app = express();
 app.use(express.json());
 
@@ -45,34 +54,27 @@ app.post('/auth/login', async (req: Request, res: Response, next) => {
 
 });
 
-app.get('/public/info', (req: Request, res: Response) => {
+app.get('/public/info',(req: Request, res: Response) => {
   res.status(200).json({ message: 'Welcome Stranger, this is public info' });
 });
 
-app.get('/protected/profile', async (req: Request, res: Response) => {
-  const token = req.header('Authorization');
-
-  if (!token ||
-    token.length === 0 ||
-    typeof token !== 'string' ||
-    token.split('.').length !==3) {
-    return res.status(401).json({ error: 'Authorization header is missing or malformed' });
-  }
-
-  const validate = await supabase.auth.getUser(token);
-  if (validate.error) {
-    return res.status(401).json({ error: 'Invalid token' });
-  }
-
-
-  return res.status(200).json({
-    id: validate.data.user.id,
-    email: validate.data.user.email,
-    account_created: validate.data.user.created_at,
-  });
-
+app.get('/protected/profile', authMiddleware, async (req: AuthRequest, res: Response) => {
+  res.status(200).json({ data: req.data });
 });
 
+app.post('/auth/logout', authMiddleware, async (req: AuthRequest, res: Response) => {
+  if (!req.token) {
+    return res.status(401).json({ error: 'No token provided' });
+  }
+  const logout = await supabase.auth.admin.signOut(req.token);
+  res.status(204).json({ message: 'Logout successful' });
+  return;
+});
+
+app.get('/protected/dashboard', authMiddleware, async (req: AuthRequest, res: Response) => {
+  res.status(200).send()
+  return
+});
 app.use((err: Error, req: Request, res: Response) => {
   res.status(500).send(err)
   return
@@ -81,3 +83,32 @@ app.use((err: Error, req: Request, res: Response) => {
 app.listen(process.env.PORT, () => {
     console.log("Server is running on port 3000 and connected to Supabase");
 });
+
+
+//Middlewares
+async function authMiddleware(req: AuthRequest, res: Response, next: () => void) {
+  const getToken = req.header('Authorization') as string;
+  if (getToken.split(' ')[0] !== 'Bearer' || !getToken || getToken.length === 0 || typeof getToken !== 'string') {
+    return res.status(401).json({ error: 'Header is missing or has invalid Authorizantio Scheme' });
+  }
+
+  const token = getToken.split(' ')[1];
+  if ( token.split('.').length !==3) {
+    return res.status(401).json({ error: 'Authorization header is missing or malformed' });
+  }
+
+  const validate = await supabase.auth.getUser(token);
+  if (validate.error) {
+    console.log(validate.error)
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+
+  req.token = token;
+  req.data ={
+    id: validate.data.user.id,
+    email: validate.data.user.email,
+    account_created: validate.data.user.created_at,
+  }
+
+  next();
+}
